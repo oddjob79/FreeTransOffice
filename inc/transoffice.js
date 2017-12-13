f2a8fc158e9b2730ca419da6cf153dd0c19ebe73
engelbert <28482378+oddjob79@users.noreply.github.com>
2017-12-13 12:09:21 -0600

    jquery additions

--- inc/transoffice.js
+++ inc/transoffice.js
@@ -1,8 +1,33 @@
-	// snippet below allows table row to become clickable and create a link from the whole row
+/*!
+ * Custom & jquery library for transoffice
+ *
+ */	
 	
+// snippet below allows table row to become clickable and create a link from the whole row
 	jQuery(document).ready(function($) {
 	$(".clickable-row").click(function() {
 		window.location = $(this).data("url");
 		});
 	});
 	
+// on select of new client in clientselect box, page is reloaded with client id set
+	jQuery(document).ready(function($) {
+	$('#clientselect').change(function() {
+		var qmark=window.location.href.indexOf("?");
+		if (qmark != -1) {
+			window.location.href = window.location.href.substring(0, qmark) +"?clientid="+(this.value);
+		} else {
+			window.location.href = window.location.href+"?clientid="+(this.value);
+		}
+		});
+	});
+
+// alert box when new button selected without filter (when needed)
+	jQuery(document).ready(function($) {
+	$('#entitylistnewbutton').click(function(e) {
+		if (window.location.href.indexOf("?clientid") == -1) {
+			e.preventDefault();
+			alert("Please select a Client before creating a new record");
+		}
+		});
+	});	
