38ae371932901c9500ba303535efc18f4981d745
engelbert <28482378+oddjob79@users.noreply.github.com>
2017-12-14 14:22:07 -0600

    Fixed issue with table moving to row below nav menu Added datatables library and implemented sorting filtering on lists

--- inc/transoffice.js
+++ inc/transoffice.js
@@ -2,13 +2,72 @@
  * Custom & jquery library for transoffice
  *
  */	
-	
+
+// utilize data tables on tables.inc	
+/*	$(document).ready( function () {
+		$('#elementlist').DataTable( {
+		  "stateSave": true,
+		  "stateSaveParams": function (settings, data) {
+			data.search.search = "";
+		  }
+		} );
+	} );
+ */
+ $(document).ready(function() {
+    $('#elementlist').DataTable( {
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.header()) )
+					.on( 'click', function(e) {
+						e.preventDefault(); 
+						e.stopPropagation();
+						return false;
+					})
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    }
+				);
+ 
+				column.data().unique().sort().each( function ( d, j ) {
+					if(column.search() === '^'+d+'$'){
+						select.append( '<option value="'+d+'" selected="selected">'+d+'</option>' )
+					} else {
+						select.append( '<option value="'+d+'">'+d+'</option>' )
+					}
+				} );
+            } );
+        },
+		"paging": true,
+		"stateSave": true,
+		"stateSaveParams": function (settings, data) {
+			data.search.search = "";
+		}
+    } );
+} );
+
 // snippet below allows table row to become clickable and create a link from the whole row
-	jQuery(document).ready(function($) {
+$(document).ready(function() {
+	var table = $('#elementlist').DataTable(); 
+	$('#elementlist tbody').on('click', 'tr',  function() {
+		window.location = $(this).data("url");
+	} );
+} );
+	 
+// snippet below allows table row to become clickable and create a link from the whole row (old - pre-datatables)
+/*	jQuery(document).ready(function($) {
 	$(".clickable-row").click(function() {
 		window.location = $(this).data("url");
 		});
 	});
+*/
 	
 // on select of new client in clientselect box, page is reloaded with client id set
 	jQuery(document).ready(function($) {
